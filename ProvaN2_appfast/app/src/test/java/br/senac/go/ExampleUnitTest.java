07eb3432f66d0b5eb4401b90e7f33d4d9921926d
rafael <rafaelreg@outlook.com>
2019-11-22 21:04:03 -0300

    upando1

---
+++ ProvaN2_appfast/app/src/test/java/br/senac/go/ExampleUnitTest.java
@@ -0,0 +1,32 @@
+package br.senac.go;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void addition_isCorrect2() {
+        assertEquals(5, 2 + 3);
+    }
+
+
+    @Test
+    public void addition_isCorrect3() {
+        assertEquals(6, 3 + 3);
+    }
+
+
+}
+
